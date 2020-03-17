2f88b5894302facf082299f254ecb1d2712101ce
Ricky <rickyalext@gmail.com>
2020-03-17 16:56:09 +0700

    save

---
+++ precache-manifest.e5118312b53f13e5fba7f5d95d77fdfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636b2234de30bfaa8e4731a5a903b28a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ca04d9995c71c709b38",
+    "url": "./static/css/main.be8f594d.chunk.css"
+  },
+  {
+    "revision": "3d30852447c0fe2677ea",
+    "url": "./static/js/2.5a31a080.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.5a31a080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca04d9995c71c709b38",
+    "url": "./static/js/main.7da3f57a.chunk.js"
+  },
+  {
+    "revision": "0940f671535f2c698db5",
+    "url": "./static/js/runtime-main.8fbc6bc4.js"
+  }
+]);
